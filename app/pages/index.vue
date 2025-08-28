b140cad38ee062981059dda79d93aebeb109e3cf
stuck-duck <louis.clemente@web.de>
2025-08-28 15:21:29 +0000

    feat: add i18n

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useI18n } from '#imports';
+
+const { locales, setLocale } = useI18n();
+</script>
+
+<template>
+  <div>
+    <button
+      v-for="locale in locales"
+      :key="locale"
+      @click="setLocale(locale.code)"
+    >
+      {{ locale.name }}
+    </button>
+    <h1>{{ $t('welcome') }}</h1>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  imports: {
+    autoImport: false,
+  },
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    defaultLocale: 'en-us',
+    locales: [
+      { code: 'en-us', name: 'English', file: 'en-us.json' },
+      { code: 'de-de', name: 'Germany', file: 'de-de.json' },
+    ],
+  },
+});
